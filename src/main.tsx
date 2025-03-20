759d7edb0a7ff4bf55873aa7155c5b50ff80e90e
SunixPN <paxa14092005@gmail.com>
2025-03-20 21:43:05 +0300

    root контейнер, правки

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { createPortal } from "react-dom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = document.getElementById("root")
+
+createRoot(root!).render(createPortal(<App />, document.querySelector("main.page-main") ?? root!));
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,20 @@ import path from 'path'
 export default defineConfig({
     plugins: [react()],
     base: "/coffee-quiz",
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: "assets/coffee-quiz.js",
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name?.endsWith('.css')) {
+                      return 'assets/coffee-quiz.css'; // Фиксированное имя для CSS
+                    }
+                    // Остальные ассеты (изображения, шрифты и т.д.) сохраняют свои имена с хэшами
+                    return 'assets/[name]-[hash][extname]';
+                  },
+            }
+        }
+    },
     resolve: {
         alias: {
             "@components": path.resolve(__dirname, "./src/components"),
